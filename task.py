b8091d57a42e1f683b15ec35396f5980f1cc12c5
thakkarbhavi10-prog <thakkarbhavi10@gmail.com>
2026-02-05 16:10:37 +0530

    this is code of pyramid

---
+++ task.py
@@ -0,0 +1,32 @@
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print("\n")
+
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print("\n")
+
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(i,end=" ")
+#     print("\n")
+
+# for i in range(1,6):
+#     for space in range (0,5-i):
+#         print(" ",end=" ")
+#     for j in range(0,(2*i-1)):
+#         print("*", end=" ")
+#
+#     print("\n")
+
+
+# for i in range(5,0,-1):
+#     for space in range (0,5-1):
+#         print(" ",end=" ")
+#     for j in range(0,(2*i-1)):
+#         print("*", end=" ")
+#
+#     print("\n")
+
